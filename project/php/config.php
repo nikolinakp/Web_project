d58aa20a728accd5ebdd340269eb6f0a7f70bef7
Nikolina Petrova <92330701+nikolinakp@users.noreply.github.com>
2024-06-06 14:11:50 +0300

    delete

--- project/php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "web_app_db");
-
-// Check connection
-if ($mysqli === false) {
-    die("ERROR: Could not connect. " . $mysqli->connect_error);
-}
-?>
-
--- project/php/history.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-
-include '../history.html';
-?>
-
-
